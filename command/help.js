9a6313c40782fed340d6ccc40cf6f38b1279c381
TuyulAkun <123469737+TuyulAkun@users.noreply.github.com>
2023-01-27 14:24:04 +0700

    Update help.js

--- command/help.js
+++ command/help.js
@@ -518,4 +518,4 @@ exports.donate = (pushname, ownerNumber) => {
 wa.me/${ownerNumber} (Owner)`
 }
 
-exports.sewanya = `Untuk List Sewa ubah sendiri di folder command, file help.js 👌 baris ke 521`
+exports.sewanya = `10k Permanen`
